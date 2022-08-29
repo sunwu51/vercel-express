8ed3ef1e088a84ae5100aea2b3e25502a2b03fc7
Frank <sunwu51@126.com>
2022-08-29 16:54:04 +0000

    add post

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {
     res.send("hello world!")
 });
 
+app.post("/post", (req, res) => {
+    res.json({
+        body: req.body,
+        query: req.query
+    })
+});
+
 app.listen(5000, ()=>{
     console.log("Listening on port 5000")
 });
